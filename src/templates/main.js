6227549547a36f6d1505504e436c1bfab7e2729f
Alexander Lindholm <alexlindholm88@gmail.com>
2023-11-08 20:45:44 +0100

    adding reusable main component

---
+++ src/templates/main.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Layout from "../layouts/layout";
+import Menu from "../templates/menu";
+
+const imageContainerStyle = {
+  display: "flex",
+  "flex-direction": "column",
+  "justify-content": "center",
+  "align-items": "center",
+  width: "100%",
+  "margin-top": "24px",
+};
+
+const Main = ({ activeTab, children }) => {
+  return (
+    <Layout>
+      <div className="background">
+        <div className="blur">
+          <div style={imageContainerStyle}>
+            <img src="/640.png" width="260" height="260" />
+            <Menu activeTab={activeTab} />
+          </div>
+          <div className="content">{children}</div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Main;
