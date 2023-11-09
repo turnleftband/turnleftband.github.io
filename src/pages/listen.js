f7c7276a1c60283e7722491fdcc75ab44534103d
Alexander Lindholm <alexlindholm88@gmail.com>
2023-11-09 19:57:14 +0100

    update follow and listen page

--- src/pages/listen.js
+++ src/pages/listen.js
@@ -1,44 +1,53 @@
 import React from "react";
 import Main from "../templates/main";
+import ButtonRow from "../templates/buttonRow";
 
 export default function Listen() {
   return (
     <Main activeTab={"listen"}>
       <div className="button-container">
-        <h3 className="button-container__title">AVAILABLE AT</h3>
-        <div className="button-row">
-          <a
-            className="button-row__image-link"
-            href="https://open.spotify.com/track/270iDGNmxJh7zw3axFFKQ7?si=c896b65dfdbc429e"
-          >
-            <img src="/spotify.png" className="spotify-row" />
-          </a>
-          <a href="https://open.spotify.com/track/270iDGNmxJh7zw3axFFKQ7?si=c896b65dfdbc429e">
-            <div className="button">LISTEN</div>
-          </a>
-        </div>
-        <div className="button-row">
-          <a
-            className="button-row__image-link"
-            href="https://www.youtube.com/watch?v=2MfHniGa-1I"
-          >
-            <img src="/youtube.png" className="youtube-row" />
-          </a>
-          <a href="https://www.youtube.com/watch?v=2MfHniGa-1I">
-            <div className="button">LISTEN</div>
-          </a>
-        </div>
-        <div className="button-row">
-          <a
-            className="button-row__image-link"
-            href="https://music.apple.com/us/album/me-myself-and-i/1458248125?i=1458248135"
-          >
-            <img src="/apple.png" className="apple-row" />
-          </a>
-          <a href="https://music.apple.com/us/album/me-myself-and-i/1458248125?i=1458248135">
-            <div className="button">LISTEN</div>
-          </a>
-        </div>
+        <h3 className="button-container__title">LISTEN</h3>
+        <ButtonRow
+          href={
+            "https://open.spotify.com/track/270iDGNmxJh7zw3axFFKQ7?si=c896b65dfdbc429e"
+          }
+          img={"/spotify.svg"}
+          title={"TURNLEFT"}
+          buttonTitle={"LISTEN"}
+        />
+        <ButtonRow
+          href={
+            "https://music.apple.com/us/album/me-myself-and-i/1458248127?i=1458248135"
+          }
+          img={"/apple.svg"}
+          title={"TURNLEFT"}
+          buttonTitle={"LISTEN"}
+        />
+        <h3 className="button-container__title">FOLLOW</h3>
+        <ButtonRow
+          href={"https://www.youtube.com/watch?v=2MfHniGa-1I"}
+          img={"/youtube.svg"}
+          title={"@TURNLEFTBAND"}
+          buttonTitle={"WATCH"}
+        />
+        <ButtonRow
+          href={"https://www.tiktok.com/@turnleftband"}
+          img={"/tiktok.svg"}
+          title={"@TURNLEFTBAND"}
+          buttonTitle={"FOLLOW"}
+        />
+        <ButtonRow
+          href={"https://instagram.com/turnleftband"}
+          img={"/instagram.svg"}
+          title={"@TURNLEFTBAND"}
+          buttonTitle={"FOLLOW"}
+        />
+        <ButtonRow
+          href={"https://facebook.com/turnleftband"}
+          img={"/facebook.svg"}
+          title={"@TURNLEFTBAND"}
+          buttonTitle={"FOLLOW"}
+        />
       </div>
     </Main>
   );
