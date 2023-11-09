070ae5d083ec9ca2c4227ea50c7943b2e52a9e3a
Alexander Lindholm <alexlindholm88@gmail.com>
2023-11-09 20:22:12 +0100

    adding wontBeBack lyrics

--- data/lyrics/index.js
+++ data/lyrics/index.js
@@ -1,12 +1,14 @@
-const meMyselfAndI = require('./meMyselfAndI');
-const myOldRecords = require('./myOldRecords');
-const moreThanThis = require('./moreThanThis');
-const sweetStockholmSyndrom = require('./sweetStockholmSyndrom');
-const taxi = require('./taxi');
-const imFine = require('./imFine');
-const godzillaPtII = require('./godzillaPtII');
+const meMyselfAndI = require("./meMyselfAndI");
+const myOldRecords = require("./myOldRecords");
+const moreThanThis = require("./moreThanThis");
+const sweetStockholmSyndrom = require("./sweetStockholmSyndrom");
+const taxi = require("./taxi");
+const imFine = require("./imFine");
+const godzillaPtII = require("./godzillaPtII");
+const wontBeBack = require("./wontBeBack");
 
 module.exports = [
+  wontBeBack,
   taxi,
   meMyselfAndI,
   myOldRecords,
---
+++ data/lyrics/wontBeBack.js
@@ -0,0 +1,79 @@
+const wontBeBack = {
+  url: "wont-be-back",
+  title: "Wont Be Back",
+  parts: [
+    `
+    Feels like everybody’s out to say
+    They’re sorry for my misery
+    But I have been all but inside that place
+    Who am I to face that you
+    Cause every day I feel us getting closer 
+    `,
+    `
+    You won’t be back
+    I can’t sit back
+    And just pretend that I don’t miss ya
+    `,
+    `
+    You won’t be back
+    I do regret
+    That I was not there for your last breath
+    `,
+    `
+    Don’t tell me to calm down cause I 
+    think I’m dying here
+    And it took me a while to process all this
+    Ignorance is bliss
+    Cause you
+    Cause every day I feel us getting closer 
+    `,
+    `
+    You won’t be back
+    I can’t sit back
+    And just pretend that I don’t miss ya
+    `,
+    `
+    You won’t be back
+    I do regret
+    That I was not there for your last breath
+    `,
+    `
+    Pretend I don’t miss ya
+    `,
+    `
+    Not there for your last breath
+    `,
+    `
+    Sorry that I didn’t stick around to the end
+    Sorry that I wasn’t a better friend
+    `,
+    `
+    Pretend I don’t miss ya
+    `,
+    `
+    Sorry that I didn’t stick around to the end
+    Sorry that I wasn’t a better friend
+    `,
+    `
+    Not there for your last breath
+    `,
+    `
+    Sorry that I didn’t stick around to the end)
+    Sorry that I wasn’t a better friend
+    (You won’t be back)
+    (I can’t sit back)
+    `,
+    `
+    And just pretend that I don’t miss ya 
+    `,
+    `
+    Sorry that I didn’t stick around to the end)
+    Sorry that I wasn’t a better friend
+    (You won’t be back)
+    (I do regret)
+    That I was not there for your last breath
+    `,
+  ],
+};
+
+module.exports = wontBeBack;
