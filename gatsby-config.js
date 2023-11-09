f76ba648ed662d531744ff0c2367af5981b00b6b
Alexander Lindholm <alexlindholm88@gmail.com>
2023-11-09 19:57:33 +0100

    adding font loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,31 @@
 module.exports = {
   siteMetadata: {
     title: `Turnleft`,
-    siteUrl: 'https://www.turnleftband.com/',
+    siteUrl: "https://www.turnleftband.com/",
   },
   plugins: [
-    'gatsby-plugin-cname',
+    "gatsby-plugin-cname",
     {
       resolve: `gatsby-plugin-output`,
       options: {
-        publicPath: 'public',
+        publicPath: "public",
         rmPublicFolder: false,
+      },
+    },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Montserrat`,
+            file: `https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap`,
+          },
+        ],
       },
     },
   ],
