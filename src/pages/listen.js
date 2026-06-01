11bb4dd29871813526c086df45fc32e9763b0172
Alexander Lindholm <alexlindholm88@gmail.com>
2026-06-01 11:36:16 +0200

    Add presave iframe widget

--- src/pages/listen.js
+++ src/pages/listen.js
@@ -2,11 +2,13 @@ import React from "react";
 import Main from "../templates/main";
 import FollowSection from "../templates/followSection";
 import ListenSection from "../templates/listenSection";
+import SocialUnlockWidget from "../templates/socialUnlockWidget";
 
 export default function Listen() {
   return (
     <Main activeTab={"listen"}>
       <div className="button-container">
+        <SocialUnlockWidget />
         <ListenSection />
         <FollowSection />
       </div>
---
+++ src/templates/socialUnlockWidget.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const SocialUnlockWidget = () => {
+  return (
+    <div className="social-unlock-widget">
+      <div className="social-unlock-widget__header">
+        <span className="social-unlock-widget__label">
+          PRESAVE THE NEW SINGLE
+        </span>
+      </div>
+      <div className="social-unlock-widget__body">
+        <iframe
+          title="Turnleft social unlock"
+          src="https://show.co/social-unlock/4PVT6x0UAc1v0iUNeB62us/widget"
+          width="100%"
+          height="300"
+          frameBorder="0"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SocialUnlockWidget;
