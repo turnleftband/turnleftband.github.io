06efb89ff89e1fc975b04904fcbbe69b574861c8
Alexander Lindholm <alexlindholm88@gmail.com>
2026-06-01 15:15:11 +0200

    link to show co instead of iframe

--- src/templates/socialUnlockWidget.js
+++ src/templates/socialUnlockWidget.js
@@ -3,20 +3,16 @@ import React from "react";
 const SocialUnlockWidget = () => {
   return (
     <div className="social-unlock-widget">
-      <div className="social-unlock-widget__header">
-        <span className="social-unlock-widget__label">
-          PRESAVE THE NEW SINGLE
+      <a className="social-unlock-widget__link" href="https://show.co/uK3fpNo">
+        <span className="social-unlock-widget__content">
+          <span className="social-unlock-widget__label">New single</span>
+          <span className="social-unlock-widget__title">Without You</span>
+          <span className="social-unlock-widget__subtitle">
+            Presave now on Spotify
+          </span>
         </span>
-      </div>
-      <div className="social-unlock-widget__body">
-        <iframe
-          title="Turnleft social unlock"
-          src="https://show.co/social-unlock/4PVT6x0UAc1v0iUNeB62us/widget"
-          width="100%"
-          height="300"
-          frameBorder="0"
-        />
-      </div>
+        <span className="social-unlock-widget__action">Presave on Spotify</span>
+      </a>
     </div>
   );
 };
